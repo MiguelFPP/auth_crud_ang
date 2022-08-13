a175f6179738a0202cd976137bf7fe457bc46448
MiguelFPP <84242304+MiguelFPP@users.noreply.github.com>
2022-08-13 13:10:23 -0500

    guard generate

---
+++ src/app/core/guards/auth.guard.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+/* para generar el verificacion */
+/* ng g guard core/guards/auth */
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return true;
+  }
+
+}
