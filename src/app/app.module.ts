1b6fec9dee8e127db810f2fddb0afab1967a15d5
MiguelFPP <84242304+MiguelFPP@users.noreply.github.com>
2022-08-13 11:31:27 -0500

    scaffolding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { PublicModule } from './public/public.module';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PublicComponent } from './public/public.component';
+import { SecureComponent } from './secure/secure.component';
+import { LoginComponent } from './public/login/login.component';
+import { RegisterComponent } from './public/register/register.component';
+import { NavbarComponent } from './public/navbar/navbar.component';
+import { ListProductsComponent } from './secure/list-products/list-products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PublicComponent,
+    SecureComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavbarComponent,
+    ListProductsComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, PublicModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
