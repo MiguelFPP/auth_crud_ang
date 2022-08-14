b858c5ff5552b7dd4423763d2efd8ff7b1fd1a40
MiguelFPP <84242304+MiguelFPP@users.noreply.github.com>
2022-08-14 13:46:40 -0500

    list and delete products

--- src/app/secure/list-products/list-products.component.ts
+++ src/app/secure/list-products/list-products.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-list-products',
   templateUrl: './list-products.component.html',
-  styleUrls: ['./list-products.component.css']
+  styleUrls: ['./list-products.component.css'],
 })
 export class ListProductsComponent implements OnInit {
+  products: [] = [];
+  load:boolean = false;
 
-  constructor() { }
+  constructor(private _productService: ProductsService) {}
 
   ngOnInit(): void {
+    this.getProducts();
   }
 
+  getProducts() {
+    this.load = true;
+    this._productService.getProducts().subscribe(
+      ({ data }) => {
+        this.products = data;
+        this.load = false;
+        console.log(this.products);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  deleteProduct(id: number) {
+    this.load = true;
+    this._productService.deleteProduct(id).subscribe(
+      (data) => {
+        this.load = false;
+        this.getProducts();
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,9 +1,33 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductsService {
+  api: string = environment.apiUrl;
+  headers!: HttpHeaders;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.headers = this.getHeaders();
+  }
+
+  getHeaders(): HttpHeaders {
+    return new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+    });
+  }
+
+  getProducts(): Observable<any> {
+    return this.http.get(`${this.api}/products`, { headers: this.headers });
+  }
+
+  deleteProduct(id: number): Observable<any> {
+    return this.http.delete(`${this.api}/products/${id}`, {
+      headers: this.headers,
+    });
+  }
 }
